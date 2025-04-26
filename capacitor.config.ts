5ba2522a21074ddb9ab814858a000959e4d2171d
jobmaria <0jobmaria0@gmail.com>
2025-04-26 12:11:47 +0100

    add inic for projet hibrido

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.jobmaria.whatsappclone',
   appName: 'Whatsapp Clone',
-  webDir: 'www'
+  webDir: 'dist/whatsaap-clone-front/browser',
+  server:{
+    androidScheme: 'https'
+  }
 };
 
 export default config;
